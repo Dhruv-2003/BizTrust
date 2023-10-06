faa4a32e9e707a6f852139f893d95df5c3d6cc41
Architsharma7 <architsharma7911@gmail.com>
2023-10-06 22:59:35 +0530

    feat: bicnomy particle auth added

---
+++ frontend/pages/onboard.tsx
@@ -0,0 +1,89 @@
+/* eslint-disable @next/next/no-assign-module-variable */
+import React, { useState } from "react";
+import { IPaymaster, BiconomyPaymaster } from "@biconomy/paymaster";
+import { IBundler, Bundler } from "@biconomy/bundler";
+import {
+  BiconomySmartAccountV2,
+  DEFAULT_ENTRYPOINT_ADDRESS,
+} from "@biconomy/account";
+import { Wallet, providers, ethers } from "ethers";
+import { ChainId } from "@biconomy/core-types";
+import { ParticleAuthModule, ParticleProvider } from "@biconomy/particle-auth";
+import {
+  ECDSAOwnershipValidationModule,
+  DEFAULT_ECDSA_OWNERSHIP_MODULE,
+} from "@biconomy/modules";
+
+const Onboard = () => {
+  const [address, setAddress] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [smartAccount, setSmartAccount] =
+    useState<BiconomySmartAccountV2 | null>(null);
+  const [provider, setProvider] = useState<ethers.providers.Provider | null>(
+    null
+  );
+
+  const particle = new ParticleAuthModule.ParticleNetwork({
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    clientKey: process.env.NEXT_PUBLIC_ClIENT_KEY,
+    appId: process.env.NEXT_PUBLIC_SERVER_KEY,
+    wallet: {
+      displayWalletEntry: true,
+      defaultWalletEntryPosition: ParticleAuthModule.WalletEntryPosition.BR,
+    },
+  });
+
+  const bundler: IBundler = new Bundler({
+    bundlerUrl:
+      "https://bundler.biconomy.io/api/v2/{80001}/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44",
+    chainId: ChainId.BASE_GOERLI_TESTNET,
+    entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
+  });
+
+  const paymaster: IPaymaster = new BiconomyPaymaster({
+    paymasterUrl: process.env.NEXT_PUBLIC_PAYMASTER_URL,
+  });
+
+  const connect = async () => {
+    try {
+      setLoading(true);
+      const userInfo = await particle.auth.login();
+      console.log("Logged in user:", userInfo);
+      const particleProvider = new ParticleProvider(particle.auth);
+      const web3Provider = new ethers.providers.Web3Provider(
+        particleProvider,
+        "any"
+      );
+      setProvider(web3Provider);
+
+      const module = await ECDSAOwnershipValidationModule.create({
+        signer: web3Provider.getSigner(),
+        moduleAddress: DEFAULT_ECDSA_OWNERSHIP_MODULE,
+      });
+
+      let biconomySmartAccount = await BiconomySmartAccountV2.create({
+        chainId: ChainId.POLYGON_MUMBAI,
+        bundler: bundler,
+        paymaster: paymaster,
+        entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
+        defaultValidationModule: module,
+        activeValidationModule: module,
+      });
+      setAddress(await biconomySmartAccount.getAccountAddress());
+      setSmartAccount(biconomySmartAccount);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      {!loading && !address && <button onClick={connect}>Connect</button>}
+      {loading && <p>Loading Smart Account...</p>}
+      {address && <h2>Smart Account: {address}</h2>}
+    </div>
+  );
+};
+
+export default Onboard;
