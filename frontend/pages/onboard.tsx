6564f69e4e86e6daa7366600482325ebd4c32224
Architsharma7 <architsharma7911@gmail.com>
2023-10-06 23:01:28 +0530

    fix: bundler changed to polygon mumbai

--- frontend/pages/onboard.tsx
+++ frontend/pages/onboard.tsx
@@ -36,7 +36,7 @@ const Onboard = () => {
   const bundler: IBundler = new Bundler({
     bundlerUrl:
       "https://bundler.biconomy.io/api/v2/{80001}/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44",
-    chainId: ChainId.BASE_GOERLI_TESTNET,
+    chainId: ChainId.POLYGON_MUMBAI,
     entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
   });
 
