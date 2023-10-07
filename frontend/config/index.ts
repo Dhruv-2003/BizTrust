716b7b47f4962e1d274f86137e60a40fda1400b2
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-10-07 16:03:33 +0530

    feat: Verify extra functions added

--- frontend/config/index.ts
+++ frontend/config/index.ts
@@ -29,3 +29,13 @@ export const ethrProvider = {
   registry: REGISTRY_CONTRACT_ADDRESS!,
   gasSource: "",
 };
+export interface JwtPayload {
+  [key: string]: any;
+  iss?: string | undefined;
+  sub?: string | undefined;
+  aud?: string | string[] | undefined;
+  exp?: number | undefined;
+  nbf?: number | undefined;
+  iat?: number | undefined;
+  jti?: string | undefined;
+}
