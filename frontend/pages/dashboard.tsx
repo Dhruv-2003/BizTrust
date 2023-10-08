057f22cdd0b3d6c2f4a09d5f26d797649cd8501c
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-10-08 22:55:13 +0530

    feat: VP functions completed

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -4,9 +4,36 @@ import Overview from "@/components/overview";
 import PaymentsHistory from "@/components/paymentsHistory";
 import Invoice from "@/components/invoice";
 import VP from "@/components/vp";
+import { getCompanyInfo } from "@/firebase/methods";
 
 const Dashboard = () => {
-  const address = ""; // get the user connected address
+  const address = "0x89D20d4f8F6F89Af9318c15f1AeFf6aB19b40247"; // get the user connected address
+  const [data, setData] = useState<{}>();
+
+  useEffect(() => {
+    if (address) {
+      if (!data) {
+        getInfo(address);
+      }
+    }
+  }, [address]);
+
+  const getInfo = async (address: `0x${string}`) => {
+    const data = await getCompanyInfo(address);
+    console.log(data);
+    setData(data);
+    // {
+    //   Name: name,
+    //   Address: companyAddress,
+    //   ContactMail: mail,
+    //   TaxNo: taxNo,
+    //   RegNo: regNo,
+    //   trustScore: 500,
+    //   invoicesIssued: [],
+    //   invoicesToPay: [],
+    // }
+    // Data will come in this way , just display as you need to and pass on the rest to the corresponding components
+  };
 
   return (
     <div className="w-screen">
@@ -26,7 +53,10 @@ const Dashboard = () => {
                     <Overview address={address} />
                   </TabPanel>
                   <TabPanel>
-                    <VP address={address} />
+                    <VP
+                      address={address}
+                      name={data ? data?.Name : "ABC Company"}
+                    />
                   </TabPanel>
                   <TabPanel>
                     <PaymentsHistory address={address} />
